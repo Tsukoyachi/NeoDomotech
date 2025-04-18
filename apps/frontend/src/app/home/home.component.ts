c206f34d7ee165398a4d7a7770e6201eda0e335e
Axel Delille <Axel.delille31@gmail.com>
2025-04-19 00:32:02 +0200

    feat: Implementation and styling of the home page

--- apps/frontend/src/app/home/home.component.ts
+++ apps/frontend/src/app/home/home.component.ts
@@ -1,10 +1,18 @@
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
+import { Router, RouterModule } from "@angular/router";
+import { MatButton } from "@angular/material/button";
 
 @Component({
   selector: "app-home",
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule, MatButton],
   templateUrl: "./home.component.html",
   styleUrl: "./home.component.css",
 })
-export class HomeComponent {}
+export class HomeComponent {
+  constructor(private router: Router) {}
+
+  goToDashboard() {
+    this.router.navigate(["/dashboard"]);
+  }
+}
